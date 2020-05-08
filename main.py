0e12ff735af5d1b787f2ec0f4f20973244f6a162
davidportlouis <dportlouis@gmail.com>
2020-05-09 00:11:39 +0530

    latest commit

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 # author: @art3mis
 
 import cv2
+import pandas as pd
 import matplotlib.pyplot as plt
 
-image = cv2.imread("test.jpg")
+user_data = pd.read_csv("sample.csv",header=None).values
+
+certi_path = "./template/test.jpg"
+
 font_size = 2
 font_color = (51,51,51)
 
@@ -11,16 +15,15 @@ y_adjustment = -730
 x_adjustment = 350
 
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
-test = "A. David Port Louis"
-event = "lorem Ipsum"
 
-text_size =  cv2.getTextSize(test,font,font_size,4)[0]
-text_x = (image.shape[1] - text_size[0])/2 + x_adjustment
-text_y = (image.shape[0] + text_size[1]/2) + y_adjustment
-text_x = int(text_x)
-text_y = int(text_y)
+for name in user_data:
+    img = cv2.imread(certi_path)
+    name = name.item()
+    text_size =  cv2.getTextSize(name,font,font_size,4)[0]
+    text_x = (img.shape[1] - text_size[0])/2 + x_adjustment
+    text_y = (img.shape[0] + text_size[1]/2) + y_adjustment
+    text_x = int(text_x)
+    text_y = int(text_y)
 
-cv2.putText(image,test,(text_x,text_y),font,font_size,font_color,4)
-cv2.imwrite("./test.png",image)
-
-# cv2.destroyAllWindows()
+    cv2.putText(img,name,(text_x,text_y),font,font_size,font_color,4)
+    cv2.imwrite(f"./{name}.png",img)
